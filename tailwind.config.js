c233ea9f1a8e88cb49e85afce600572025387960
Ljupce003 <Ljupcebrusli1@gmail.com>
2026-03-13 02:57:04 +0100

    added improved UI to index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-  purge: [],
-  content: ['./**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
+  content: [
+    './*.html',
+    './Assets/**/*.html',
+    './projects/**/*.html',
+    './templates/**/*.html',
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          950: '#060d1f', // You can change this to your preferred navy hex
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
